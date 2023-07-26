dd7e4ef0ac9f86ff64bb153f7bcafc30eedf3405
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-07-26 12:04:25 +0600

    learned difference between slice, substr, substring

---
+++ 11-Javascript/string.js
@@ -0,0 +1,21 @@
+"use strict";
+let str = "I love Pallabi";
+console.log(str.substring(7, 14));
+console.log(str.substr(7, 12));
+console.log(str.substr(7, 7));
+console.log(str.slice(7, 14));
+console.log("hello");
+console.log(str.slice(-7, -3));
+console.log("hello");
+console.log(str.slice(-7));
+// console.log(str.substr(7, 14));
+
+const ss = "Apple,* juice, misti";
+console.log(ss.charAt(0));
+console.log(ss.charCodeAt(5));
+console.log(ss.split(""));
+console.log(ss.split("i"));
+console.log(ss.split(" "));
+console.log(ss.split(","));
+console.log(ss.substr(2,5));
+console.log(ss.substr(2,20));
