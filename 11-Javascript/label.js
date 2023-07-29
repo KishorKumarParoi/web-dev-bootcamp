2524ff0dc3c43be24ab652c45411b9003b117d71
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-07-29 11:28:13 +0600

    learned label

---
+++ 11-Javascript/label.js
@@ -0,0 +1,12 @@
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let text = "";
+
+list: {
+    text += cars[0] + "<br>";
+    text += cars[1] + "<br>";
+    break list;
+    text += cars[2] + "<br>";
+    text += cars[3] + "<br>";
+}
+
+console.log(text);
