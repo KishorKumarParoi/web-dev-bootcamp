a7f3dd4b6e10d6f28fe0b6851d5a0c59970f36da
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-07-29 11:27:36 +0600

    learned if-else

---
+++ 11-Javascript/if-else.js
@@ -0,0 +1,11 @@
+const time = new Date().getHours();
+let greeting;
+if (time < 10) {
+    greeting = "Good morning";
+} else if (time < 20) {
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+
+console.log(greeting);
