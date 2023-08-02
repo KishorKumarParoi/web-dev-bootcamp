509801e58de4bdda36f29e419d61c6a95d395375
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-02 16:55:51 +0600

    learned named import

---
+++ 11-Javascript/modules.js
@@ -0,0 +1,25 @@
+// ? JavaScript modules allow you to break up your code into separate files.
+
+// * in-line individually
+export const pi = 3.141592;
+export const e = 2.718281;
+const Name = "Kishor";
+export default Name;
+
+// * all at once 
+// const name = "Jesse";
+// const age = 40;
+
+// export { name, age };
+
+// const message = () => {
+//     const name = "Jesse";
+//     const age = 40;
+//     return name + ' is ' + age + 'years old.';
+// };
+
+// export default message;
+
+export const myfunc = function () {
+    return "Hello";
+}
