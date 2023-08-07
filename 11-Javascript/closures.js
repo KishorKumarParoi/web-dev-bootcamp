dd0ac182a4361bff44f5bd854c609125033dda2b
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-07 19:37:11 +0600

    self-closing function

--- 11-Javascript/closures.js
+++ 11-Javascript/closures.js
@@ -1,18 +1,21 @@
-var num1 = 10;
-var k = 100;
+(function () {
+    var num1 = 10;
+    var k = 100;
+    let num = 189;
 
-var sum = function balance(intialBalance) {
-    var num2 = 20;
-    var balance = intialBalance;
-    return function () {
-        balance += num1 + num2 + k;
-        return balance;
-    };
-}
+    var sum = function balance(intialBalance) {
+        var num2 = 20;
+        var balance = intialBalance;
+        return function () {
+            balance += num1 + num2 + k + num;
+            return balance;
+        };
+    }
 
-var myFunc = sum(10000);
-// console.log(sum());
-console.dir(myFunc);
+    var myFunc = sum(10000);
+    // console.log(sum());
+    console.dir(myFunc);
+})()
 
 
 
