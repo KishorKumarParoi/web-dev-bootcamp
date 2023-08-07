da575012ee0772e82f0872dd299558c76a9d2ea4
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-07 19:40:52 +0600

    only module and closure

--- 11-Javascript/closures.js
+++ 11-Javascript/closures.js
@@ -17,23 +17,32 @@
 //     console.dir(myFunc);
 // })()
 
-var num1 = 10;
-var k = 100;
-let num = 189;
+// var num1 = 10;
+// var k = 100;
+// let num = 189;
 
-var sum = function balance(intialBalance) {
-    var num2 = 20;
-    var balance = intialBalance;
+// var sum = function balance(intialBalance) {
+//     var num2 = 20;
+//     var balance = intialBalance;
+//     return function () {
+//         balance += num1 + num2 + k + num;
+//         return balance;
+//     };
+// }
+
+// var myFunc = sum(10000);
+// console.log(sum());
+// console.dir(myFunc);
+
+
+let x = 10;
+let jog = function () {
+    let y = 20;
     return function () {
-        balance += num1 + num2 + k + num;
-        return balance;
+        return x + y;
     };
 }
 
-var myFunc = sum(10000);
-// console.log(sum());
-console.dir(myFunc);
-
-
+console.dir(jog());
 
 debugger;
