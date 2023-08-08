13138cdcf230c178c794e3947225fe482e094489
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-08 14:36:08 +0600

    learned hoisting in let

---
+++ 11-Javascript/hoisting2.js
@@ -0,0 +1,39 @@
+// var a;
+// a = undefined;
+// console.log(a);
+// a = 40;
+
+// let x;
+// console.log(x);
+// x = undefined; x = 10; let x = 10;
+
+var a;
+console.log(a);
+// console.dir(a);
+a = 40;
+console.log(a);
+
+let x; // x = undefined
+console.log(x);
+
+let language = "JavaScript";
+let LANGUAGE = "Python";
+
+function getLanguage() {
+    if (!language) {
+        let language = "Ruby";
+        // console.log(language);
+    }
+    return language;
+}
+
+console.log(getLanguage());
+
+debugger;
+
+
+
+// * Hoisting means that the JavaScript interpreter will move all 
+// * variable and function declarations to the top of the current scope.
+// * var a; at the top of the scope.
+// * a = undefined; like placeholder in html by javascript
