4339139bc9f5ad692e00097c88a653f82318e9ec
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-10 01:24:48 +0600

    learned about rest parameter

--- 11-Javascript/function-parameter.js
+++ 11-Javascript/function-parameter.js
@@ -4,4 +4,16 @@ function myFunc(x, y = 10) {
 
 myFunc(10);
 
+function sumAll(...args) {
+    let sum = 0;
+    let length = arguments.length;
+    for (let i = 0; i < length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+}
+
+
+console.log(sumAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
 debugger;
