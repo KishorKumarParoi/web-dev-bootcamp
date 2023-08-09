06c99befc239945853b9791eb0e696f77144cca3
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-10 01:25:44 +0600

    learned about function default object 'arguments'

--- 11-Javascript/function-parameter.js
+++ 11-Javascript/function-parameter.js
@@ -5,12 +5,15 @@ function myFunc(x, y = 10) {
 myFunc(10);
 
 function sumAll(...args) {
-    let sum = 0;
+    let sum = 0, max = -Infinity;
     let length = arguments.length;
     for (let i = 0; i < length; i++) {
         sum += arguments[i];
+        if (arguments[i] > max) {
+            max = arguments[i];
+        }
     }
-    return sum;
+    return { sum, max };
 }
 
 
