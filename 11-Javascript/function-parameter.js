a1cbfab2d6a0cb99b56edbbea99d41adf49be831
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-10 01:41:45 +0600

    learned difference of passing by reference and passing by values through arguments and object

--- 11-Javascript/function-parameter.js
+++ 11-Javascript/function-parameter.js
@@ -1,5 +1,5 @@
 function myFunc(x, y = 20) {
-    console.log(arguments);
+    // console.log(arguments);
     console.log(x + y);
 }
 
@@ -19,6 +19,23 @@ function sumAll(...args) {
 
 
 console.log(sumAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+function a(x, y) {
+    x.one = 7;
+    y = 69;
+    return x.one * x.two * y;
+}
+
+const m = {
+    one: 1,
+    two: 2
+};
+
+const z = 100;
+
+console.log(a(m, z));
+console.log(m);
+console.log(z);
 
 // ? Arguments are Passed by Value
 // ? Changes to arguments are not visible(reflected) outside the function.
