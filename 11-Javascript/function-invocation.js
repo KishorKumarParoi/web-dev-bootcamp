c2a2855c7f265ec15115ae30a5012a5805f2180b
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-11 10:43:23 +0600

    invoking function as a function and window.mul and mul() is same for global object

---
+++ 11-Javascript/function-invocation.js
@@ -0,0 +1,14 @@
+// Javascript Function can be invoked without being called
+// There are 4 ways to invoke a function in Javascript
+// 1. Function Invocation
+// 2. Method Invocation
+// 3. Constructor Invocation
+// 4. Indirect Invocation
+
+// invoking a function as a function
+function mul(a, b) {
+    return a * b;
+}
+
+console.log(mul(1, 2));
+// console.log(window.mul(10, 20));
