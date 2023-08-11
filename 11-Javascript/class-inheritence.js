e22f766702daf704fe84f05f39aabd01e24dbfb5
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-11 19:07:48 +0600

    added getter/setter and renamed this.carname to this._carname

--- 11-Javascript/class-inheritence.js
+++ 11-Javascript/class-inheritence.js
@@ -1,9 +1,17 @@
 class Car {
     constructor(brand) {
-        this.carname = brand;
+        this._carname = brand;
     }
     present() {
         return 'I have a ' + this.carname;
+    }
+
+    get carname() {
+        return this._carname;
+    }
+
+    set carname(name) {
+        this._carname = name;
     }
 }
 
@@ -26,5 +34,12 @@ console.log(myCar.show());
 
 // ? Inheritance is useful for code reusability: reuse properties and methods of an existing class when you create a new class.
 // ? To create a class inheritance, use the extends keyword.
+
+
+const newCar = new Car('Toyota');
+console.log(newCar.carname);
+
+newCar.carname = 'Tesla';
+console.log(newCar.carname);
 
 debugger;
