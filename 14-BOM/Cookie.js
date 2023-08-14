a9b0a869224ca1bef82461500b799f64099d5482
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-14 14:05:48 +0600

    setCookie and getCookie

---
+++ 14-BOM/Cookie.js
@@ -0,0 +1,37 @@
+const setName = document.getElementById('setName');
+const setUserName = document.getElementById('setUserName');
+const getName = document.getElementById('getName');
+const getUserName = document.getElementById('getUserName');
+
+// console.log(setName, setUserName, getName, getUserName);
+// setName.innerHTML = "Kishor";
+// console.log(setName);
+document.cookie = null;
+console.log(document.cookie);
+
+function setCookie(cname, cval, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    document.cookie = cname + "=" + cval + ";expires=" + d.toUTCString() + ";path=/";
+}
+
+function getCookie(cname) {
+    let ca = document.cookie.split(';');
+    console.log(ca);
+
+    let length = ca.length;
+    let name = cname + "=";
+    console.log(cname);
+
+    for (let i = 0; i < length; i++) {
+        console.log(ca[i]);
+        let str = ca[i].trim();
+        console.log(str);
+
+        if (str.indexOf(name) === 0) {
+            return str.substring(name.length);
+        }
+    }
+
+    return "";
+}
