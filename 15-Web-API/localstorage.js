ce80240c8e96a2374e2ca96083d7b79e2fcd572b
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-15 00:20:27 +0600

    made up 4 functions set, get, remove, clear

---
+++ 15-Web-API/localstorage.js
@@ -0,0 +1,15 @@
+function setLocalStorageItem(key, value) {
+    localStorage.setItem(key, value);
+}
+
+function getLocalStorageItem(key) {
+    return localStorage.getItem(key);
+}
+
+function removeLocalStorageItem(key) {
+    localStorage.removeItem(key);
+}
+
+function clearLocalStorage() {
+    localStorage.clear();
+}
