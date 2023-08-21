af0f0984181092e1f1ed72191001c083d61397bf
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-21 18:06:44 +0600

    created new task and addnewtask function

--- 18-Advanced-Dom-Projects/to-do-app/JS/script.js
+++ 18-Advanced-Dom-Projects/to-do-app/JS/script.js
@@ -1,14 +1,31 @@
 let newTask = document.getElementById("new-task");
 let items = document.getElementById("items");
 let completeItems = document.getElementById("completed-items");
+let submit = document.getElementById("submit");
+let demo = document.getElementById("demo");
 
-console.log(newTask);
-console.log(items);
-console.log(completeItems);
+// console.log(newTask);
+// console.log(items);
+// console.log(completeItems);
 
-console.log("hello world");
+// console.log("hello world");
 
 // Add a new task
-let addTask = function () {
+let addNewTask = function () {
+    // event.preventDefault();
+    let newTaskValue = newTask.value;
+    createNewTask(newTaskValue);
+    // demo.innerHTML = newTaskValue;
+};
 
-}
+let createNewTask = function (task) {
+    let listItem = document.createElement("li");
+    let checkBox = document.createElement("input");
+    let label = document.createElement("label");
+
+    checkBox.type = "checkbox";
+    label.innerText = task;
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+    items.appendChild(listItem);
+};
