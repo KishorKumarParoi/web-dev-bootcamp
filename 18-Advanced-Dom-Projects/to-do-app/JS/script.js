6eb8ade950b8c80a2eebd420590f7abd222f0965
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-21 17:56:16 +0600

    linked js file and increased font size

---
+++ 18-Advanced-Dom-Projects/to-do-app/JS/script.js
@@ -0,0 +1,14 @@
+let newTask = document.getElementById("new-task");
+let items = document.getElementById("items");
+let completeItems = document.getElementById("completed-items");
+
+console.log(newTask);
+console.log(items);
+console.log(completeItems);
+
+console.log("hello world");
+
+// Add a new task
+let addTask = function () {
+
+}
