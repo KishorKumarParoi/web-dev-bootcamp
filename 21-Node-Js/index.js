9c0f037da8f8e233c2eafce23c6434194d4bdaef
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-22 00:29:43 +0600

    packgae.json modified

--- 21-Node-Js/index.js
+++ 21-Node-Js/index.js
@@ -1,8 +1,17 @@
 var a = 10;
+let bb = {
+  firstname: "kishor",
+};
+
 console.log(module.filename);
 console.log(module.paths);
 console.log(module);
 // console.log(__.pathname);
 // export default a;
+
+module.exports = {
+  a,
+  bb,
+};
 
 debugger;
--- 21-Node-Js/sum.js
+++ 21-Node-Js/sum.js
@@ -1,4 +1,8 @@
-import a from "./index.js";
+// import a from "./index.js";
+let a = require("./index.js");
 
 var b = 100;
-console.log(a * b);
+console.log(a);
+console.log(a.bb);
+console.log(a.a);
+debugger;
