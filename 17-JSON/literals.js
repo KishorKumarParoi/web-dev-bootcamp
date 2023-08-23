1877f9522cf0a831bf1c02d2fd6cf300dd7cfe4e
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-23 22:44:54 +0600

    object literal vs json

---
+++ 17-JSON/literals.js
@@ -0,0 +1,15 @@
+const myJSON = '{"name":"John", "age":30, "car":null}';
+const myObj = JSON.parse(myJSON);
+
+let text = "";
+for (const x in myObj) {
+  text += x + ", ";
+}
+
+console.log(text);
+
+// object literals and json are not the same
+// object literals are not json
+// json is not object literals
+// json is a string
+// json is a string that looks like an object literal but is not an object literal and is not an object and is not a function and is not a date
