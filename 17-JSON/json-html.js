c240fd6749e582dc3092e351d6beef6c3b0ca60a
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-24 00:34:59 +0600

    fetching post request method learned

---
+++ 17-JSON/json-html.js
@@ -0,0 +1,16 @@
+async function change_myselect(sel) {
+  const dbParam = JSON.stringify({ table: sel, limit: 20 });
+  const response = await fetch(
+    "https://www.w3schools.com/js/json_demo_html_table.php",
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+      body: "x=" + dbParam,
+    }
+  );
+  const data = await response.json();
+}
+
+change_myselect("customers");
