8e6a13e2d95844b27df9f8d8a0f631250ca2bdaf
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-23 22:55:51 +0600

    array literal vs json

--- 17-JSON/literals.js
+++ 17-JSON/literals.js
@@ -13,3 +13,11 @@ console.log(text);
 // json is not object literals
 // json is a string
 // json is a string that looks like an object literal but is not an object literal and is not an object and is not a function and is not a date
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(typeof arr);
+const jsonArr = JSON.stringify(arr);
+console.log(jsonArr);
+
+debugger;
