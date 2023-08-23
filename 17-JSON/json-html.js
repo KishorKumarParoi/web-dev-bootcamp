8add810bda0b14daeda05d38591daf534dc04e76
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-24 00:42:20 +0600

    json file address

--- 17-JSON/json-html.js
+++ 17-JSON/json-html.js
@@ -1,15 +1,12 @@
 async function change_myselect(sel) {
   const dbParam = JSON.stringify({ table: sel, limit: 20 });
-  const response = await fetch(
-    "https://www.w3schools.com/js/json_demo_html_table.php",
-    {
-      method: "POST",
-      headers: {
-        "Content-type": "application/x-www-form-urlencoded",
-      },
-      body: "x=" + dbParam,
-    }
-  );
+  const response = await fetch("json_demo_html_table.php", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: "x=" + dbParam,
+  });
   const data = await response.json();
 }
 
