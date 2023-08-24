88041bdf55f2f46e8e35f7c1ca880d5dc6f7d073
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-24 20:40:31 +0600

    creating methods with Person.prototype and object.create()

--- 20-Advanced-Javascript-Concepts/prototype.js
+++ 20-Advanced-Javascript-Concepts/prototype.js
@@ -12,7 +12,25 @@
 //   },
 // };
 
-const permanentMethods = {
+// const permanentMethods = {
+//   eat() {
+//     console.log("Person is eating");
+//   },
+//   sleep() {
+//     console.log("Person is sleeping");
+//   },
+// };
+
+function Person(name, age) {
+  const person = Object.create(Person.prototype);
+  person.age = age;
+  person.name = name;
+  //   person.eat = permanentMethods.eat;
+  //   person.sleep = permanentMethods.sleep;
+  return person;
+}
+
+Person.prototype = {
   eat() {
     console.log("Person is eating");
   },
@@ -21,20 +39,22 @@ const permanentMethods = {
   },
 };
 
-function Person(name, age) {
-  const person = {};
-  person.age = age;
-  person.name = name;
-  person.eat = permanentMethods.eat;
-  person.sleep = permanentMethods.sleep;
-  return person;
-}
-
 const Kishor = Person("Kishor", 24);
 const JoeRogan = Person("Joe Rogan", 54);
 
 console.log(Kishor.name);
 JoeRogan.sleep();
 console.log(JoeRogan);
+
+const captain = {
+  name: "Captain America",
+  power: "Shield",
+  getPower() {
+    return this.power;
+  },
+};
+
+const tonyStark = Object.create(captain);
+console.log(tonyStark.power);
 
 debugger;
