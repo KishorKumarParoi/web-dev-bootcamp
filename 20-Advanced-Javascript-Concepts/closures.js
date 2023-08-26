46b91302f52e1c60126b708d3f14d94ab7104d8c
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-26 09:04:12 +0600

    timer delay function learned

--- 20-Advanced-Javascript-Concepts/closures.js
+++ 20-Advanced-Javascript-Concepts/closures.js
@@ -19,3 +19,22 @@ closureFunction(); // Output: "I'm from outer scope"
 
 console.dir(closureFunction);
 // debugger;
+
+function stopWatch() {
+  let startTime = Date.now();
+  return function getDelay() {
+    console.log(Date.now() - startTime);
+  };
+}
+
+let timer = stopWatch();
+// for (let i = 0; i < 100000000; i++) {
+//   let a = Math.random() * 10000000;
+// }
+
+timer(); // Output: Time elapsed: 1000ms
+console.log(timer);
+
+timer = null;
+timer();
+debugger;
