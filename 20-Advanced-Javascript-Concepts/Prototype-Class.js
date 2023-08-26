859455a0fb2e7c888d783d06a5e8089b20755e53
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-26 19:28:21 +0600

    Person.prototype

--- 20-Advanced-Javascript-Concepts/Prototype-Class.js
+++ 20-Advanced-Javascript-Concepts/Prototype-Class.js
@@ -1,28 +1,26 @@
 function Person(name, age) {
   this.name = name;
   this.age = age;
-  this.play = () => {
-    console.log(`${this.name} is playing`);
-  };
 }
 
-function Cricketer(name, type, country) {
+function Cricketer(type, country) {
   this.type = type;
-  this.name = name;
   this.country = country;
-  this.eat = () => {
-    console.log(`${this.name} is eating`);
-  };
 }
 
-Cricketer.prototype.name = (name) => {
-  this.name = name;
+Person.prototype.eat = () => {
+  console.log(`${this.name} is eating`);
 };
 
-const kkp = new Cricketer("Kishor", "All Rounder", "Bangladesh");
-console.log(kkp);
-kkp.eat("kishor");
+Cricketer.prototype = {
+  play: () => {
+    console.log(`${this.name} is playing`);
+  },
+};
+
+const sakib = new Cricketer("All Rounder", "Bangladesh");
+console.log(sakib);
 
 const tamim = new Person("Tamim", 32);
 console.log(tamim);
-tamim.play();
+debugger;
