5df8c04ab2c47b92b3f4749623f7f278bc95aa96
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-27 08:33:25 +0600

    class PersonNew, class CricketerNew is the son

--- 20-Advanced-Javascript-Concepts/Prototype-Class.js
+++ 20-Advanced-Javascript-Concepts/Prototype-Class.js
@@ -40,4 +40,42 @@ console.log(tamim);
 tamim.kishor();
 tamim.eat();
 
+console.log("converting whole code to class constructor");
+
+class PersonNew {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  eat() {
+    console.log(`${this.name} is eating`);
+  }
+}
+
+class CricketerNew extends PersonNew {
+  constructor(name, age, type, country) {
+    super(name, age);
+    this.age = age;
+    this.name = name;
+    this.type = type;
+    this.country = country;
+  }
+
+  play() {
+    console.log(`${this.name} is playing`);
+  }
+}
+
+CricketerNew.prototype.sleep = function () {
+  console.log(`${this.name} is sleeping`);
+};
+
+const Shanto = new CricketerNew("Shanto", 32, "Batsman", "Bangladesh");
+console.log(Shanto);
+Shanto.sleep();
+
+const Soumya = new PersonNew("Soumya", 28);
+console.log(Soumya);
+
 debugger;
