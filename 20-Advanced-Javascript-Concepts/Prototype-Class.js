ce231f9d3429898fe126afc7c7dc6ade91fadda8
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-27 08:24:44 +0600

    learning inheritance in prototype

--- 20-Advanced-Javascript-Concepts/Prototype-Class.js
+++ 20-Advanced-Javascript-Concepts/Prototype-Class.js
@@ -3,26 +3,34 @@ function Person(name, age) {
   this.age = age;
 }
 
-function Cricketer(type, country) {
+function Cricketer(name, age, type, country) {
+  // making connection with Person
+  Person.call(this);
+  this.name = name;
+  this.age = age;
+
   this.type = type;
   this.country = country;
 }
 
-Person.prototype.eat = (name) => {
+Person.prototype.eat = function () {
   console.log(`${this.name} is eating`);
-};
-
-Cricketer.prototype = {
-  play: function (type) {
-    console.log(`${this.type} is playing`);
-  },
 };
 
 Object.prototype.kishor = () => {
   console.log("Kishor");
 };
 
-const sakib = new Cricketer("All Rounder", "Bangladesh");
+Cricketer.prototype = Object.create(Person.prototype);
+Cricketer.prototype.constructor = Cricketer;
+
+Cricketer.prototype = {
+  play: function () {
+    console.log(`${this.name} is playing`);
+  },
+};
+
+const sakib = new Cricketer("Sakib", 34, "All Rounder", "Bangladesh");
 console.log(sakib);
 sakib.kishor();
 sakib.play();
