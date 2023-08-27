137647cd0673342907d508a36ccce97f92419ae8
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-27 09:22:37 +0600

    passing a object to function and then implicit binding calling

--- 20-Advanced-Javascript-Concepts/this-call-bind-apply.js
+++ 20-Advanced-Javascript-Concepts/this-call-bind-apply.js
@@ -20,3 +20,24 @@ let sakib = {
 };
 
 sakib.printPlayerNameFunction();
+
+let printPlayerNameFunction2 = function (obj) {
+  obj.printPlayerNameFunction = function () {
+    console.log(`${this.name} is ${this.age} years old`);
+  };
+};
+
+let sakib2 = {
+    name: "Sakib",
+    age: 34,
+  },
+  tamim2 = {
+    name: "Tamim",
+    age: 32,
+  };
+
+printPlayerNameFunction2(sakib2);
+printPlayerNameFunction2(tamim2);
+
+sakib2.printPlayerNameFunction();
+tamim2.printPlayerNameFunction();
