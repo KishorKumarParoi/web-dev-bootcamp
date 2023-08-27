3373e550358f6f72bb50786b3518f7e44c54caa2
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-27 09:31:02 +0600

    shanto.father.printName() here this denotes to father

--- 20-Advanced-Javascript-Concepts/this-call-bind-apply.js
+++ 20-Advanced-Javascript-Concepts/this-call-bind-apply.js
@@ -41,3 +41,37 @@ printPlayerNameFunction2(tamim2);
 
 sakib2.printPlayerNameFunction();
 tamim2.printPlayerNameFunction();
+
+let Person = function (name, age) {
+  return {
+    name: name,
+    age: age,
+    printName: function () {
+      console.log(this.name);
+    },
+    father: {
+      name: "Mr. X",
+      age: 50,
+      printName: function () {
+        console.log(this.name);
+      },
+      grandfather: {
+        name: "Mr. Y",
+        age: 80,
+        printName: function () {
+          console.log(`${this.name} is ${this.age} years old`);
+        },
+      },
+    },
+  };
+};
+
+let Shanto = Person("Shanto", 24);
+Shanto.printName();
+console.log(Shanto.age);
+
+Shanto.father.printName();
+console.log(Shanto.father.age);
+
+Shanto.father.grandfather.printName();
+console.log(Shanto.father.grandfather.age);
