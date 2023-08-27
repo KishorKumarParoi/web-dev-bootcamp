f80a292397a8e7bdcfff028ebc9598decfccdfa0
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-27 21:22:00 +0600

    converted value to boolean

--- 19-ES6-JS-Interview-Questions/1_interview.js
+++ 19-ES6-JS-Interview-Questions/1_interview.js
@@ -4,3 +4,8 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, "", null, undefined, NaN, 0, false];
 let newArr = arr.filter(Boolean);
 console.log(newArr);
+
+// convert any value to boolean
+let x = "Kishor";
+console.log(Boolean(x));
+console.log(!!x);
