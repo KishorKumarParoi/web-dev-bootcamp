d03bc7e5ca78db64f3c1741eb28e05f7ef22cc95
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-28 23:26:18 +0600

    comma operator on a variable

---
+++ 19-ES6-JS-Interview-Questions/comma-operator.js
@@ -0,0 +1,8 @@
+// comma operator
+// comma operator is used to evaluate each of its operands from left to right and returns the value of the last operand.
+
+let a = 10;
+a = (a, --a);
+console.log(a);
+
+debugger;
