aae5e79c08abb8fbab2f7622fede506f7fd03808
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-29 19:08:17 +0600

    getting strings and substituted values as array

--- 19-ES6-JS-Interview-Questions/3_interview.js
+++ 19-ES6-JS-Interview-Questions/3_interview.js
@@ -14,7 +14,22 @@ function myFunc(x) {
   return x;
 }
 
-const str = myFunc`Hello`;
+const str = myFunc`Hello + World`;
 console.log(str);
+// console.log(str.raw);
+// console.log(str.arguments);
+
+function customTag(strings, ...values) {
+  console.log(strings); // Array of string literals
+    console.log(values); // Array of substituted values
+    
+}
+
+const text = "world";
+const txt2 = "world2";
+const txt3 = "world3";
+const txt4 = "world4";
+
+customTag`Hello ${text} ${txt2} ${txt3} ${txt4} !`;
 
 debugger;
