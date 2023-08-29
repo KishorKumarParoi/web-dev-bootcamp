59587c80f1f32ba9bd1d76d38c424d7504c9e6d9
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-29 18:56:22 +0600

    learned tagged template literal

--- 19-ES6-JS-Interview-Questions/3_interview.js
+++ 19-ES6-JS-Interview-Questions/3_interview.js
@@ -8,4 +8,13 @@ console.log(true + "5");
 console.log(true + 5 + "5");
 console.log(true + 5 + "5" + true);
 console.log(true / 5 + "5" * true - 5);
+
+function myFunc(x) {
+  //   return "Bangladesh";
+  return x;
+}
+
+const str = myFunc`Hello`;
+console.log(str);
+
 debugger;
