67e02bdfd4863ac4f1fe84ba2d77cee1a01f71bb
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-30 10:45:38 +0600

    getting array from arguments

--- 19-ES6-JS-Interview-Questions/4_interview.js
+++ 19-ES6-JS-Interview-Questions/4_interview.js
@@ -4,4 +4,20 @@ console.log("Bangladesh".__proto__); // String
 let x = new String("Bangladesh");
 console.dir(x);
 
+// make a function that sorts its arguments without using loops
+
+function sortArguments(...args) {
+  //   for (let arg of args) {
+  //     console.log(arg);
+  //   }
+  console.log(args);
+
+  let arr = Array.from(args);
+  let arr2 = [...args];
+  console.log(arr.sort((a, b) => a - b));
+  console.log(arr2);
+}
+
+sortArguments(5, 3, 7, 1); // [1, 3, 5, 7]
+
 debugger;
