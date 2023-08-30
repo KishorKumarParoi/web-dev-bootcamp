ee9e4a356240fbba341d1662666732d48d662695
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-30 11:20:01 +0600

    learned short circuit OR Method and solved with other two ways ternary operator and if-else

--- 19-ES6-JS-Interview-Questions/5_interview.js
+++ 19-ES6-JS-Interview-Questions/5_interview.js
@@ -11,8 +11,24 @@ const fruits = [
 
 const uniqueFruits = [...new Set(fruits)];
 console.log(uniqueFruits); // ["apple", "banana", "orange", "kiwi", "mango"]
-
 console.log(fruits);
 
+function countMap(data) {
+  let output = {};
+
+  data.map((fruit) => {
+    // output[fruit] = output[fruit] ? output[fruit] + 1 : 1;
+    output[fruit] = (output[fruit] || 0) + 1;
+    // if (output[fruit] !== undefined) {
+    //   output[fruit]++;
+    // } else {
+    //   output[fruit] = 1;
+    // }
+  });
+
+  return output;
+}
+
+console.log(countMap(fruits));
 
 debugger;
