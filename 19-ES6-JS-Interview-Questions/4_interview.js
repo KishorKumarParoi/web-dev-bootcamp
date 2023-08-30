71b2ab29b0521d0d2975490b9dd72c8de04aa06b
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-30 10:40:34 +0600

    learned __proto__ returning

---
+++ 19-ES6-JS-Interview-Questions/4_interview.js
@@ -0,0 +1,7 @@
+console.log("Bangladesh".__proto__.__proto__.__proto__); // null
+console.log("Bangladesh".__proto__.__proto__); // Object
+console.log("Bangladesh".__proto__); // String
+let x = new String("Bangladesh");
+console.dir(x);
+
+debugger;
