cd10bd45ab2728bc03bdcc80895770dc9d0d8dac
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-31 21:09:07 +0600

    interview questions practicing

---
+++ 19-ES6-JS-Interview-Questions/6_interview.js
@@ -0,0 +1,29 @@
+// interview questions
+
+// 1. What is the output?
+const user = {
+  name: "John",
+  years: 30,
+};
+
+const { name, years: age, isAdmin = false } = user;
+
+console.log(name); // John
+
+console.log(age); // 30
+
+console.log(isAdmin); // false
+
+// 2. What is the output?
+
+let salaries = {
+    John: 100,
+    Pete: 300,
+    Mary: 250,
+},
+    sum = 0;
+
+for (let salary of Object.values(salaries)) {
+    sum += salary;
+}
+
