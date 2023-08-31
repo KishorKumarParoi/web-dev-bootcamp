6e12f4779ef91b343c934d35ca936f1e31a184f2
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-31 21:09:54 +0600

    got sum printed from object values

--- 19-ES6-JS-Interview-Questions/6_interview.js
+++ 19-ES6-JS-Interview-Questions/6_interview.js
@@ -20,10 +20,11 @@ let salaries = {
     John: 100,
     Pete: 300,
     Mary: 250,
-},
-    sum = 0;
+  },
+  sum = 0;
 
 for (let salary of Object.values(salaries)) {
-    sum += salary;
+  sum += salary;
 }
 
+console.log(sum);
