e0239fbbe5a919422add743bb4af59c806d14238
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-01 20:35:19 +0600

    output debug:

--- 19-ES6-JS-Interview-Questions/7_interview.js
+++ 19-ES6-JS-Interview-Questions/7_interview.js
@@ -2,3 +2,15 @@
 fetch("https://jsonplaceholder.typicode.com/todos/1").then((response) =>
   console.log(response.json())
 );
+
+// 3. What is the output?
+const user = {
+  name: "John",
+  years: 30,
+};
+const { name: Name, years: age, isAdmin = false } = user;
+
+console.log(user.name);
+console.log(user.years);
+console.log(Name);
+console.log(age);
