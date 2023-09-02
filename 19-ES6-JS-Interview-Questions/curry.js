ac364f511a6fce65bdaf4fec95d57c2698cc958e
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-02 08:57:34 +0600

    we make function part by part and can handle seperately

--- 19-ES6-JS-Interview-Questions/curry.js
+++ 19-ES6-JS-Interview-Questions/curry.js
@@ -12,4 +12,9 @@ function curriedMultiply(a) {
   };
 }
 
-console.log(multiply(10, 20, 30));
+let step1 = curriedMultiply(10);
+let step2 = step1(22);
+let step3 = 6 * step2(30);
+
+// console.log(multiply(10, 20, 30));
+console.log(step3);
