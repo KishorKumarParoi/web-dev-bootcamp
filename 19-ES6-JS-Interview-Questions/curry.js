04799dc452b1e9ab24baccc6ea9468deab856633
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-03 08:26:47 +0600

    curried Converter

--- 19-ES6-JS-Interview-Questions/curry.js
+++ 19-ES6-JS-Interview-Questions/curry.js
@@ -65,15 +65,27 @@ console.log(product3);
 
 // curry converter
 
-function curry(func) {}
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args2.concat(args));
+      };
+    }
+  };
+}
 
 function sum(a, b, c) {
   return a + b + c;
 }
 
-let curriedSum = curry(sum);
-console.log(curriedSum);
+console.log(sum.length);
 
+let curriedSum = curry(sum);
+
+console.log(curriedSum(1, 2, 3));
 console.log(curriedSum(1)(2, 3));
 console.log(curriedSum(1)(2)(3));
 console.log(curriedSum(1, 2)(3));
