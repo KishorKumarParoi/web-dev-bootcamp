8d132c281d30619e40b92ff8e90a4543e8f28bf1
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-05 22:09:40 +0600

    learned curry converter deeply and partial function also

--- 19-ES6-JS-Interview-Questions/currying.js
+++ 19-ES6-JS-Interview-Questions/currying.js
@@ -1,3 +1,6 @@
+// Currying is a technique of evaluating function with multiple arguments, into sequence of functions with single argument.
+const x = require("lodash");
+
 function multiply(a) {
   return function (b) {
     return function (c) {
@@ -12,7 +15,29 @@ let step3 = step2(30);
 
 console.dir(step1);
 console.dir(step2);
-
 console.log(step3);
+
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args2.concat(args));
+      };
+    }
+  };
+}
+
+function sum(a, b, c, d, e, f) {
+  return a + b + c + d + e + f;
+}
+
+let curryConverter = curry(sum);
+
+console.dir(curryConverter(10, 20, 30));
+console.dir(curryConverter(10)(20, 30));
+console.dir(curryConverter(10)(20)(30));
+console.dir(curryConverter(10)(20)(30)(40)(50)(60));
 
 debugger;
