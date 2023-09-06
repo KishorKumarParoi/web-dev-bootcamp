295aecc11e93704794ca6aac1723f3f02d9498d2
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-07 00:10:55 +0600

    normal function returns undefined for  because this belongs window

--- 19-ES6-JS-Interview-Questions/fat-arrow.js
+++ 19-ES6-JS-Interview-Questions/fat-arrow.js
@@ -2,8 +2,11 @@ let javascript = {
   Name: "JavaScript",
   libraries: ["React", "Angular", "Vue"],
   printLibraries: function () {
-    this.libraries.forEach((value, index, arr) => {
-      console.log(`${this.Name} loves ${value}!`);
+    // this.libraries.forEach((value, index, arr) => {
+    //   console.log(`${this.Name} loves ${value}!`);
+    // });
+    this.libraries.forEach(function (a) {
+      console.log(`${this.Name} loves ${a}!`);
     });
   },
 };
