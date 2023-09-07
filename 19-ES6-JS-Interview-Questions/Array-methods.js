4a89783240a29302df94b21f4e91ae9a8af0358d
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-07 18:05:38 +0600

    traversing and finding arr value getter than 4

--- 19-ES6-JS-Interview-Questions/Array-methods.js
+++ 19-ES6-JS-Interview-Questions/Array-methods.js
@@ -1,16 +1,29 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-  this.fullName = this.name + " " + this.age;
-  this.fullName = () => {
-    return this.name + " " + this.age;
+  test() {
+    console.log("test");
+  }
+
+  fullName = function () {
+    return `${this.name} ${this.age}`;
+  };
+
+  exampleFunction = function () {
+    let arr = [1, 2, 3, 4, 5];
+    let val = arr.find(function (ele) {
+      return ele > 4;
+    });
+    return val;
   };
 }
 
 const kishor = new Person("kishor", 25);
 let x = kishor.fullName();
 console.log(x); // kishor 25
-console.log(kishor.age); // 25
+console.log(kishor.exampleFunction()); // 1 2 3 4 5
 
 debugger;
