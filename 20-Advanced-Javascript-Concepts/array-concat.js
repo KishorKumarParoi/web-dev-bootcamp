240454735b39d8c50695b651cb86dba6b91f1b98
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-07 22:09:33 +0600

    concatenated more arrays

--- 20-Advanced-Javascript-Concepts/array-concat.js
+++ 20-Advanced-Javascript-Concepts/array-concat.js
@@ -1,7 +1,9 @@
 const arr = [1, 2, 3, 4, 5, 6, 8, 100];
 const arr2 = [3, 4, 5, 2];
+const arr3 = [1, 23];
+const arr4 = [20, 204];
 
-const result = arr.concat(arr2);
+const result = arr.concat(arr2, arr3, arr4);
 console.log(result); // [1, 2, 3, 4, 5, 6, 8, 100, 3, 4, 5, 2]
 console.log(arr2); // [3, 4, 5, 2]
 console.log(arr); // [1, 2, 3, 4, 5, 6, 8, 100]
