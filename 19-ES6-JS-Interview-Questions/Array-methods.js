89531d78976f375cc7746e77c8c6004b7658201e
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-07 18:15:43 +0600

    array binding this and arrow function this

--- 19-ES6-JS-Interview-Questions/Array-methods.js
+++ 19-ES6-JS-Interview-Questions/Array-methods.js
@@ -22,12 +22,36 @@ class Person {
     }, this);
     return val;
   };
+
+  exampleFunction2 = function () {
+    let arr = [1, 2, 3, 4, 5];
+    let val = arr.find(
+      function (ele) {
+        console.log(this);
+        console.log(this.test());
+        console.log(this.fullName());
+        return ele > 4;
+      }.bind(this)
+    );
+    return val;
+  };
+
+  exampleFunction3 = function () {
+    let arr = [1, 2, 3, 4, 5];
+    let val = arr.find((ele) => {
+      console.log(this);
+      console.log(this.test());
+      console.log(this.fullName());
+      return ele > 4;
+    });
+    return val;
+  };
 }
 
 const kishor = new Person("kishor", 25);
 let x = kishor.fullName();
 console.log(x); // kishor 25
 console.log(kishor.exampleFunction()); // 1 2 3 4 5
-// kishor.exampleFunction();
-
+console.log(kishor.exampleFunction2());
+console.log(kishor.exampleFunction3());
 debugger;
