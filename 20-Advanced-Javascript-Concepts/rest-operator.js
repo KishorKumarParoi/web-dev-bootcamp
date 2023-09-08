60839ac7f337b62872135e7188444e760e667572
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-08 14:54:37 +0600

    destructuring nested elements with rest operator

--- 20-Advanced-Javascript-Concepts/rest-operator.js
+++ 20-Advanced-Javascript-Concepts/rest-operator.js
@@ -8,4 +8,18 @@ function sum(...nums) {
 
 console.log(sum(1, 2, 3, 4, 5));
 
+const [c, ...a] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(a);
+console.log(c);
+
+const { name, age, ...restInfo } = {
+  name: "Alice",
+  age: 30,
+  city: "New York",
+  country: "USA",
+};
+console.log(name); // 'Alice'
+console.log(age); // 30
+console.log(restInfo); // { city: 'New York', country: 'USA' }
+
 debugger;
