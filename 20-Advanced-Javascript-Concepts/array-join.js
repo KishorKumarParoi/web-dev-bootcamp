fb11700c12d3076ee828c3fa6b2c1e280a16bd89
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-08 13:58:22 +0600

    split method makes array from string

--- 20-Advanced-Javascript-Concepts/array-join.js
+++ 20-Advanced-Javascript-Concepts/array-join.js
@@ -2,5 +2,14 @@ const arr = [12, 3, 4, 5, 6, 7, 8, 9, 10];
 let str = arr.join();
 console.log(str);
 
-str = arr.join(' ');
+str = arr.join(" ");
 console.log(str);
+
+let arr2 = arr.join("$");
+console.log(arr2);
+console.log(typeof arr2);
+
+let convertArr2 = arr2.split("$");
+console.log(convertArr2);
+
+debugger;
