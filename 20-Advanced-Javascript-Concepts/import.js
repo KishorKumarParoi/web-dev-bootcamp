b4e31b6f6f7f265150d002e95fa6679421f37f48
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-11 19:32:54 +0600

    dynamic imprt using async/await

--- 20-Advanced-Javascript-Concepts/import.js
+++ 20-Advanced-Javascript-Concepts/import.js
@@ -1,3 +1,10 @@
-import { Hello, World } from "./export.js";
-Hello();
-World();
+// import { Hello, World } from "./export.js";
+// Hello();
+// World();
+
+(async function () {
+  const { Hello: hello } = await import("./export.js");
+  const { World: world } = await import("./export.js");
+  hello();
+  world();
+})();
