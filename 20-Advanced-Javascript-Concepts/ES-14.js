bf63850db912e0f59190ea84650d2ed988f5e1b1
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-11 21:34:32 +0600

    other immutable reverse methods

--- 20-Advanced-Javascript-Concepts/ES-14.js
+++ 20-Advanced-Javascript-Concepts/ES-14.js
@@ -1,5 +1,11 @@
 // reverse Array Immutable Way
 const arr = [1, 2, 3, 4, 5];
-const reversedArr = arr.toReversed();  // es-14 feature
+// const reversedArr = arr.toReversed();  // es-14 feature
+let reversedArr = [...arr].reverse();
+reversedArr = arr.slice().reverse();
+
+// using reduce
+reversedArr = arr.reduce((prev, curr) => [curr, ...prev], []);
+
 console.log(reversedArr);
 console.log(arr);
