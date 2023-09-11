9df7f7f59344b3570796c1910181828bf408d3a8
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-11 17:53:14 +0600

    max integer limit reached

--- 20-Advanced-Javascript-Concepts/ES-11.js
+++ 20-Advanced-Javascript-Concepts/ES-11.js
@@ -11,3 +11,10 @@ globalThis.setTimeout(() => {
 globalThis.setTimeout(() => {
   console.log("Hello World from Global This!");
 }, 4000);
+
+let largestNumber = Number.MAX_SAFE_INTEGER;
+console.log(largestNumber);
+++largestNumber;
+console.log(largestNumber);
+largestNumber++;
+console.log(largestNumber); // doesn't work/increase as it is the largest number
