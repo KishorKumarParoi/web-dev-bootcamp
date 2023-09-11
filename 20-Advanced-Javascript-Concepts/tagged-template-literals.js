a357047ec69cd2de6e72c22f2fbbd2d425d7c7db
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-11 11:08:36 +0600

    tagged template literals for printing text

--- 20-Advanced-Javascript-Concepts/tagged-template-literals.js
+++ 20-Advanced-Javascript-Concepts/tagged-template-literals.js
@@ -2,6 +2,15 @@ let text;
 function modifier(strings, ...values) {
   console.log(strings);
   console.log(values);
+  let i = 1;
+  text = strings.reduce((prev, curr) => {
+    return (
+      prev +
+      curr +
+      (values.length ? (i === 1 ? (i--, "Mr. ") : "") + values.shift() : "")
+    );
+    i--;
+  }, "");
 }
 
 const person1 = "Kishor";
@@ -9,5 +18,6 @@ const sector1 = "Blockchain";
 const sector2 = "AI";
 
 modifier`Hello ${person1}, be best in ${sector1} and ${sector2} fields.`;
+console.log(text);
 
 debugger;
