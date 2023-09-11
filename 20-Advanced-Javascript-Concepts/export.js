7a5ec5db77fa2237570212295cad7bc2b1a2df26
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-11 10:54:06 +0600

    exported function

--- 20-Advanced-Javascript-Concepts/export.js
+++ 20-Advanced-Javascript-Concepts/export.js
@@ -2,3 +2,10 @@ export const pi = 3.14159;
 export const e = 2.71828;
 const phi = 1.61803;
 export default phi;
+
+function myFunc() {
+  console.log("Hello World KKP");
+}
+
+const name = "Kishor";
+export { myFunc, name };
--- 20-Advanced-Javascript-Concepts/import.js
+++ 20-Advanced-Javascript-Concepts/import.js
@@ -1,3 +1,6 @@
-import external, { pi, e } from "./export.js";
+import external, { pi, e, name, myFunc } from "./export.js";
 console.log(pi, e); // 3.14159
 console.log(external); // 1.61803
+
+myFunc(); // Hello World KKP
+console.log(name); // Kishor
