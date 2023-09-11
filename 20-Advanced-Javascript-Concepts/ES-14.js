6721e6eaccbcf85d1962a298339ae614b3764373
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-11 21:47:09 +0600

    es-14 feature of toSpliced

--- 20-Advanced-Javascript-Concepts/ES-14.js
+++ 20-Advanced-Javascript-Concepts/ES-14.js
@@ -1,4 +1,4 @@
-// reverse Array Immutable Way
+// Reverse Array Immutable Way
 const arr = [1, 2, 3, 4, 5];
 // const reversedArr = arr.toReversed();  // es-14 feature
 let reversedArr = [...arr].reverse();
@@ -8,4 +8,10 @@ reversedArr = arr.slice().reverse();
 reversedArr = arr.reduce((prev, curr) => [curr, ...prev], []);
 
 console.log(reversedArr);
+console.log(arr);
+
+// Splice Array in Immutable Way
+
+let SplicedArr = arr.toSpliced(2, 2, 100, 200); // es-14 feature
+console.log(SplicedArr);
 console.log(arr);
