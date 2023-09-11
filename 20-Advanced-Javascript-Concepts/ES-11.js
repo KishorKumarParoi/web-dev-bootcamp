0ac1f5b5c17644f95892d0550012a32ab7e50738
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-11 17:55:57 +0600

    big int introduced in ES_11

--- 20-Advanced-Javascript-Concepts/ES-11.js
+++ 20-Advanced-Javascript-Concepts/ES-11.js
@@ -18,3 +18,10 @@ console.log(largestNumber);
 console.log(largestNumber);
 largestNumber++;
 console.log(largestNumber); // doesn't work/increase as it is the largest number
+
+let biggestNumber = BigInt(Number.MAX_SAFE_INTEGER);
+console.log(biggestNumber);
+++biggestNumber;
+console.log(biggestNumber);
+biggestNumber *= biggestNumber;
+console.log(biggestNumber);
