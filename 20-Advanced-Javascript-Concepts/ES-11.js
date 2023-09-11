ddef4f8bda1afe6f8dd30a1b1710faac05dfbd7e
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-11 18:01:21 +0600

    operation of bigInt

--- 20-Advanced-Javascript-Concepts/ES-11.js
+++ 20-Advanced-Javascript-Concepts/ES-11.js
@@ -1,32 +1,40 @@
-console.log("Hello World Jonogon!");
+// console.log("Hello World Jonogon!");
 
-window.setTimeout(() => {
-  console.log("Hello World from Window!");
-}, 2000);
+// window.setTimeout(() => {
+//   console.log("Hello World from Window!");
+// }, 2000);
 
-globalThis.setTimeout(() => {
-  console.log("Hello World from Node Js!");
-}, 3000);
+// globalThis.setTimeout(() => {
+//   console.log("Hello World from Node Js!");
+// }, 3000);
 
-globalThis.setTimeout(() => {
-  console.log("Hello World from Global This!");
-}, 4000);
+// globalThis.setTimeout(() => {
+//   console.log("Hello World from Global This!");
+// }, 4000);
 
-let largestNumber = Number.MAX_SAFE_INTEGER;
-console.log(largestNumber);
-++largestNumber;
-console.log(largestNumber);
-largestNumber++;
-console.log(largestNumber); // doesn't work/increase as it is the largest number
+// let largestNumber = Number.MAX_SAFE_INTEGER;
+// console.log(largestNumber);
+// ++largestNumber;
+// console.log(largestNumber);
+// largestNumber++;
+// console.log(largestNumber); // doesn't work/increase as it is the largest number
 
-let biggestNumber = BigInt(Number.MAX_SAFE_INTEGER);
-console.log(biggestNumber);
-++biggestNumber;
-console.log(biggestNumber);
-biggestNumber *= biggestNumber;
-console.log(biggestNumber);
+// let biggestNumber = BigInt(Number.MAX_SAFE_INTEGER);
+// console.log(biggestNumber);
+// ++biggestNumber;
+// console.log(biggestNumber);
+// biggestNumber *= biggestNumber;
+// console.log(biggestNumber);
 
-let num = 10n;
-num += 100n;
-console.log(num);
+// let num = 10n;
+// num += 100n;
+// console.log(num);
 
+let num1 = 10n;
+let num2 = 20n;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
--- 20-Advanced-Javascript-Concepts/tempCodeRunnerFile.js
+++ 20-Advanced-Javascript-Concepts/tempCodeRunnerFile.js
@@ -1,1 +1,17 @@
-"Mango"
+let largestNumber = Number.MAX_SAFE_INTEGER;
+// console.log(largestNumber);
+// ++largestNumber;
+// console.log(largestNumber);
+// largestNumber++;
+// console.log(largestNumber); // doesn't work/increase as it is the largest number
+
+// let biggestNumber = BigInt(Number.MAX_SAFE_INTEGER);
+// console.log(biggestNumber);
+// ++biggestNumber;
+// console.log(biggestNumber);
+// biggestNumber *= biggestNumber;
+// console.log(biggestNumber);
+
+// let num = 10n;
+// num += 100n;
+// console.log(num);
