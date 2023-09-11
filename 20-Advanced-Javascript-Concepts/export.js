6ead87e4900b69519ba79ab1c27f99209883b6c0
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-11 10:55:13 +0600

    used template literal for printing function value in export.js

--- 20-Advanced-Javascript-Concepts/export.js
+++ 20-Advanced-Javascript-Concepts/export.js
@@ -3,8 +3,8 @@ export const e = 2.71828;
 const phi = 1.61803;
 export default phi;
 
-function myFunc() {
-  console.log("Hello World KKP");
+function myFunc(name) {
+  console.log(`Hello World ${name}!`);
 }
 
 const name = "Kishor";
--- 20-Advanced-Javascript-Concepts/import.js
+++ 20-Advanced-Javascript-Concepts/import.js
@@ -2,5 +2,5 @@ import external, { pi, e, name, myFunc } from "./export.js";
 console.log(pi, e); // 3.14159
 console.log(external); // 1.61803
 
-myFunc(); // Hello World KKP
+myFunc(name); // Hello World KKP
 console.log(name); // Kishor
