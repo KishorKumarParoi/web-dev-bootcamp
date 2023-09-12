5c5349bfcb49d71c9cf3f229d3360ee6a9423ce4
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-12 21:48:04 +0600

    implementation of Symbol includes

--- 20-Advanced-Javascript-Concepts/Symbol.js
+++ 20-Advanced-Javascript-Concepts/Symbol.js
@@ -18,5 +18,15 @@ const obj = {
 for (let item in obj) {
   console.log(item);
 }
-
 console.log(obj);
+
+const myArray = [1, 2, 3, 4, 5];
+let includes = Symbol("My Own Array Includes Method");
+Array.prototype[includes] = function () {
+  console.log("This value exists in the array");
+};
+
+console.log(myArray);
+console.log(myArray.includes(4));
+console.log(myArray["includes"](3));
+myArray[includes](2);
