f97f7bf263a3cdd6f0955f5e2163468c955778bf
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-12 21:31:56 +0600

    Array for this object -Symbol

--- 20-Advanced-Javascript-Concepts/Symbol.js
+++ 20-Advanced-Javascript-Concepts/Symbol.js
@@ -5,15 +5,17 @@ console.log(x === y); // false
 console.log(x);
 console.log(y);
 
+const arr = Symbol("Array for this Object");
 const obj = {
   name: "Kishor",
   surName: "Paroi",
   age: 25,
+  [arr]: [1, 2, 3, 4, 5],
   Nationality: "Indian",
   [x]: "world",
 };
 
-for (item in obj) {
+for (let item in obj) {
   console.log(item);
 }
 
