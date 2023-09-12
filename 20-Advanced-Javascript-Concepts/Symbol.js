030a8248958bcc539ddcc00b69e098fee4f2893f
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-12 21:10:09 +0600

    Symbol hides elements using Symbol

--- 20-Advanced-Javascript-Concepts/Symbol.js
+++ 20-Advanced-Javascript-Concepts/Symbol.js
@@ -4,3 +4,17 @@ const y = Symbol.for("hello");
 console.log(x === y); // false
 console.log(x);
 console.log(y);
+
+const obj = {
+  name: "Kishor",
+  surName: "Paroi",
+  age: 25,
+  Nationality: "Indian",
+  [x]: "world",
+};
+
+for (item in obj) {
+  console.log(item);
+}
+
+console.log(obj);
