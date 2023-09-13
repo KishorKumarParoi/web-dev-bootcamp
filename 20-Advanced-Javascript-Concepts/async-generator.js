037b277ea3fc56b408e44f97a0489bdb9277bf0f
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-13 21:31:10 +0600

    generator solution for ordering food

--- 20-Advanced-Javascript-Concepts/async-generator.js
+++ 20-Advanced-Javascript-Concepts/async-generator.js
@@ -1,31 +1,39 @@
 console.log("async generator");
 
-const takeOrder = (customer, callback) => {
+const takeOrder = (customer) => {
   setTimeout(() => {
     console.log(`Taking order for ${customer}`);
-    callback(customer);
   }, 1000);
 };
 
-const processOrder = (customer, callback) => {
+const processOrder = (customer) => {
   setTimeout(() => {
     console.log(`Processing order for ${customer}`);
-    callback(customer);
   }, 1000);
 };
 
-const completeOrder = (customer, callback) => {
+const completeOrder = (customer) => {
   setTimeout(() => {
     console.log(`Completed order for ${customer}`);
-    callback(customer);
   }, 1000);
 };
 
-takeOrder("Kishor", (customer) => {
-  processOrder(customer, (customer) => {
-    completeOrder(customer, (customer) => {
-      console.log(`Order Served ${customer}`);
-    });
-  });
-});
+// takeOrder("Kishor", (customer) => {
+//   processOrder(customer, (customer) => {
+//     completeOrder(customer, (customer) => {
+//       console.log(`Order Served ${customer}`);
+//     });
+//   });
+// });
 
+function* solution(customer) {
+  yield takeOrder(customer);
+  yield processOrder(customer);
+  yield completeOrder(customer);
+}
+
+let iterator = solution("Kishor");
+console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
