f2d773352a6c1809bef6154026e4051318267cfd
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-14 00:28:29 +0600

    intersection observer with api

---
+++ 20-Advanced-Javascript-Concepts/intersection-observer-api.js
@@ -0,0 +1,20 @@
+let circle = document.getElementById("circle");
+// circle.style.color = "blue";
+// console.log(circle.style.color);
+
+const observer = new IntersectionObserver(
+  (items) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        console.log(`Item ${item.target.id} is visible`);
+      } else {
+        console.log(`Item ${item.target.id} is not visible`);
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+observer.observe(circle);
