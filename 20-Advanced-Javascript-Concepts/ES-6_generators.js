cd3bf82f2cfb6633007c477978ff0e33a7c513ee
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-13 20:26:00 +0600

    made obj like iterables with Symbol.iterator

--- 20-Advanced-Javascript-Concepts/ES-6_generators.js
+++ 20-Advanced-Javascript-Concepts/ES-6_generators.js
@@ -17,10 +17,41 @@ console.log(iterator.next());
 // Making a object iterable
 
 const obj = {
-  value1: 1,
-  value2: 2,
-  value3: 3,
+  value1: 10,
+  value2: 20,
+  value3: 30,
 };
 
-console.log(Object.entries(obj));
-obj[Symbol.iterator] = function () {};
+// console.log(Object.entries(obj));
+
+obj[Symbol.iterator] = function () {
+  let index = 0;
+  let arr = Object.entries(this);
+  let length = arr.length;
+  console.log(arr);
+
+  return {
+    next() {
+      if (index < length) {
+        let result = {
+          key: arr[index][0],
+          value: arr[index][1],
+          done: false,
+        };
+        index++;
+        return result;
+      }
+      return {
+        done: true,
+      };
+    },
+  };
+};
+
+let iterator2 = obj[Symbol.iterator]();
+console.log(iterator2.next());
+console.log(iterator2.next());
+console.log(iterator2.next());
+console.log(iterator2.next());
+
+console.log([...obj]);
