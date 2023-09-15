1f1b12714f61550986f1a59878ea2768ad3a2ff9
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-15 09:20:45 +0600

    worker.js works fine

--- 20-Advanced-Javascript-Concepts/worker.js
+++ 20-Advanced-Javascript-Concepts/worker.js
@@ -1,2 +1,10 @@
-console.log("---------Web Worker-----------");
-
+onmessage = function (message) {
+  console.log(message.data);
+  console.log("Worker has started working...");
+  let total = 0;
+  for (let i = 0; i < 10000000000; i++) {
+    total += i;
+  }
+  console.log("Worker has finished working...");
+  postMessage(total);
+};
